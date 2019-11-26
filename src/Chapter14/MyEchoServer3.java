5aace797b2afc55758c2a7f9823a5da06e8e7331
Dodanto <ohho0728@naver.com>
2019-11-26 14:42:22 +0900

    Nov 26th

---
+++ src/Chapter14/MyEchoServer3.java
@@ -0,0 +1,58 @@
+package Chapter14;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MyEchoServer3 {
+
+	public static int DEFAULT_PORT = 7;
+
+	public static void main(String[] args) {
+		try (ServerSocket ss = new ServerSocket(DEFAULT_PORT)) {
+			while (true) {
+				try {
+					Socket sock = ss.accept();
+					System.out.println("접속을 기다립니다.");
+					new MyEchoThread(sock).start();
+				} catch (Exception e) {
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class MyEchoThread extends Thread{
+	
+	Socket sock;
+	
+	public MyEchoThread(Socket s) {
+		sock = s;
+	}
+	public void run() {
+		try {
+			System.out.println(sock.getInetAddress() + " " + sock.getPort() + "로부터의 접속요청 ");
+
+			PrintWriter pw = new PrintWriter(sock.getOutputStream());
+			BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			System.out.println("문자 읽기 ");
+
+			String str = null;
+			while ((str = br.readLine()) != null) {
+				System.out.println(str);
+				pw.println(str);
+				pw.flush();
+			}
+			pw.close();
+			br.close();
+			sock.close();
+		} catch (Exception e) {
+		}
+	}
+}
